9da2fc2507786dde1ebd4552c74c7d0028a045a5
Joaquin Roibal <jroibal@nmt.edu>
2016-04-20 13:33:45 -0600

    Added def __getitem__, def __setitem__,

--- Sequencing-Class-Example-Student-List.py
+++ Sequencing-Class-Example-Student-List.py
@@ -1,4 +1,7 @@
 class student():
+	"""
+	The purpose of this class python program is to show 'sequencing', which treats an object as a list and can be indexed.
+	"""
 	def __init__(self, lst = []):
 		self.stdList = lst
 	def __repr__(self):
@@ -13,6 +16,14 @@ class student():
 			returnStr = returnStr + i
 		return returnStr
 		
-s = student()
-s2 = student(['Amy', 'Betty'])
+	def __getitem__(self, key):
+		return self.stdList[key]
+	
+	def __setitem__(self, key, content):
+		self.stdList[key] = content
+		
+#s = student()
+s = student(['Amy', 'Betty'])
+s[1] = "Cindy"
+print(s[1])
   
